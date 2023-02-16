357b3cc6ea81ab7be6d96fd4ef59110bb79e4d97
yeah1tsmark <marcwafula635@gmail.com>
2023-02-16 09:52:39 +0300

    adding css

--- djangoProjectMark/settings.py
+++ djangoProjectMark/settings.py
@@ -113,11 +113,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
